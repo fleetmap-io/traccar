42c0d4ea51cc7277aef66c0d5b7c33d172983198
Joaquim Cardeira <joaquim.cardeira@gmail.com>
2026-05-18 15:33:30 +0100

    TYPE_CUSTOM

--- src/main/java/org/traccar/protocol/HuabaoProtocol.java
+++ src/main/java/org/traccar/protocol/HuabaoProtocol.java
@@ -24,6 +24,7 @@ public class HuabaoProtocol extends BaseProtocol {
 
     public HuabaoProtocol() {
         setSupportedDataCommands(
+                Command.TYPE_CUSTOM,
                 Command.TYPE_ENGINE_STOP,
                 Command.TYPE_ENGINE_RESUME);
         addServer(new TrackerServer(false, getName()) {
